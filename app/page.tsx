f47079a14398cf5e9fc90a44c52cbd844cbe5f46
Zyggzz <86673777+Zyggzzz@users.noreply.github.com>
2024-11-21 13:34:02 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import discordlogo from "@/app/assets/images/discordlogo.png";
+
 export default function Home() {
   return (
     <div>
-      <Image src={discordlogo.src} alt="discord logo" />
       <Link href="/contact" prefetch={true} />
     </div>
   );
